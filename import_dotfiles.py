5c6e6b3fa6b738713080c779d770131b503d738a
Peter Janak <2010janak2010@gmail.com>
2017-12-30 14:12:18 +0000

    feat: Now requires path to config file as argument

--- import_dotfiles.py
+++ import_dotfiles.py
@@ -1,24 +1,24 @@
 #!python3
 
+from sys import argv, exit
 from pathlib import Path
 from shutil import copyfile
 from socket import gethostname
 from os import makedirs, path
 import json
-home = str(Path.home())
 
-settings = None
+args = argv[1:]
 
-with open(path.join(path.dirname(path.realpath(__file__)), 'settings.json'), 'r') as f:
-    settings = json.load(f)
 
 def import_configs():
+    home = str(Path.home())
+    settings = get_config_file(args[0])
     config_dir = settings['dest_dir']
     file_to_import = settings['colect_files']
     for conf in file_to_import:
         current_config_dir = path.join(home, config_dir, gethostname())
-        
-        copy_file = input('Overwrite: ' + path.join(conf[0]) + ' (y/n) ')
+
+        copy_file = input('Overwrite: ' + path.join(conf[0]) + ' [ Default (n)o ]\n(y)es (n)o (e)xit : ')
 
         if copy_file == 'y':
             if len(conf) > 2:
@@ -26,9 +26,26 @@ def import_configs():
                 copyfile(path.join(opt_dir, conf[1]), path.join( home, conf[0]))
             else:
                 copyfile(path.join(current_config_dir, conf[1]), path.join( home, conf[0]))
-            
+
             print ('Copied...')
+        elif copy_file == 'e':
+            exit()
         else:
             print ('Skipped...')
 
-import_configs()
+
+
+def get_config_file(path_to_file):
+    try:
+        with open(path_to_file) as f:
+            return json.load(f)
+    except FileNotFoundError as e:
+        print('Error: File does not exists')
+        exit()
+
+
+
+if not len(args):
+    print('Requires path to config file as an argument')
+else:
+    import_configs()
